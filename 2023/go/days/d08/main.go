bb925310683967b3de9ed2b00ddca0aafef0cff4
david-littlefarmer <david.littlefarmer@gmail.com>
2023-12-08 10:43:57 +0100

    d08

--- 2023/go/days/d08/main.go
+++ 2023/go/days/d08/main.go
@@ -2,7 +2,9 @@ package d08
 
 import (
 	"fmt"
+	"strings"
 	"time"
+	"unicode"
 )
 
 func Run(input []string) (string, time.Duration, string, time.Duration) {
@@ -15,7 +17,8 @@ func Run(input []string) (string, time.Duration, string, time.Duration) {
 func PartOne(input []string) (string, time.Duration) {
 	start := time.Now()
 
-	result := parseInput(input)
+	instructions, nodes := parseInput(input)
+	result := part1(instructions, nodes)
 
 	return fmt.Sprint(result), time.Since(start)
 }
@@ -23,11 +26,94 @@ func PartOne(input []string) (string, time.Duration) {
 func PartTwo(input []string) (string, time.Duration) {
 	start := time.Now()
 
-	result := parseInput(input)
+	instructions, nodes := parseInput(input)
+	result := part2(instructions, nodes)
 
 	return fmt.Sprint(result), time.Since(start)
 }
 
-func parseInput(input []string) (result int) {
+type node struct {
+	l string
+	r string
+}
+
+func parseInput(input []string) (instructions string, nodes map[string]node) {
+	instructions = input[0]
+
+	nodes = make(map[string]node, len(input)-2)
+	for i := 2; i < len(input); i++ {
+		f := func(c rune) bool {
+			return !unicode.IsLetter(c)
+		}
+
+		fields := strings.FieldsFunc(input[i], f)
+		n := node{
+			l: fields[1],
+			r: fields[2],
+		}
+
+		nodes[fields[0]] = n
+	}
+
+	return instructions, nodes
+}
+
+func part1(instructions string, nodes map[string]node) (steps int) {
+	current := "AAA"
+	for current != "ZZZ" {
+		for _, c := range instructions {
+			if c == 'L' {
+				current = nodes[current].l
+			} else {
+				current = nodes[current].r
+			}
+
+			steps++
+		}
+	}
+
+	return steps
+}
+
+func part2(instructions string, nodes map[string]node) int {
+	var starters []string
+	for c := range nodes {
+		if c[2] == 'A' {
+			starters = append(starters, c)
+		}
+	}
+
+	steps := make([]int, len(starters))
+	for i, current := range starters {
+		for current[2] != 'Z' {
+			for _, c := range instructions {
+				if c == 'L' {
+					current = nodes[current].l
+				} else {
+					current = nodes[current].r
+				}
+
+				steps[i]++
+			}
+		}
+	}
+
+	result := 1
+	for _, s := range steps {
+		result = lcm(result, s)
+	}
+
 	return result
+}
+
+func lcm(a, b int) int {
+	return (a * b) / gcd(a, b)
+}
+
+func gcd(a, b int) int {
+	for b != 0 {
+		a, b = b, a%b
+	}
+
+	return a
 }
