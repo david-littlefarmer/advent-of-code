f9e984381ee0386c5a84c5022c647e6c28d7c630
david-littlefarmer <david.littlefarmer@gmail.com>
2023-12-09 09:03:22 +0100

    d09

--- 2023/go/days/d09/main.go
+++ 2023/go/days/d09/main.go
@@ -2,7 +2,10 @@ package d09
 
 import (
 	"fmt"
+	"strings"
 	"time"
+
+	"advent-of-code/helpers"
 )
 
 func Run(input []string) (string, time.Duration, string, time.Duration) {
@@ -15,7 +18,11 @@ func Run(input []string) (string, time.Duration, string, time.Duration) {
 func PartOne(input []string) (string, time.Duration) {
 	start := time.Now()
 
-	result := parseInput(input)
+	reports := parseInput(input)
+	var result int
+	for _, r := range reports {
+		result += processReport(reverseSlice(r))
+	}
 
 	return fmt.Sprint(result), time.Since(start)
 }
@@ -23,11 +30,72 @@ func PartOne(input []string) (string, time.Duration) {
 func PartTwo(input []string) (string, time.Duration) {
 	start := time.Now()
 
-	result := parseInput(input)
-
+	reports := parseInput(input)
+	var result int
+	for _, r := range reports {
+		result += processReport(r)
+	}
 	return fmt.Sprint(result), time.Since(start)
 }
 
-func parseInput(input []string) (result int) {
-	return result
+func parseInput(input []string) (reports [][]int) {
+	for _, s := range input {
+		nums := strings.Fields(s)
+		report := make([]int, 0, len(nums))
+		for _, n := range nums {
+			report = append(report, helpers.ParseInt(n))
+		}
+
+		reports = append(reports, report)
+	}
+
+	return reports
+}
+
+func reverseSlice(s []int) []int {
+	length := len(s)
+	reversed := make([]int, length)
+
+	for i := 0; i < length; i++ {
+		reversed[i] = s[length-i-1]
+	}
+
+	return reversed
+}
+
+func processReport(report []int) (result int) {
+	lines := buildTree(report)
+
+	first := lines[len(lines)-1][0]
+	for i := len(lines) - 2; i >= 0; i-- {
+		first = lines[i][0] - first
+	}
+
+	return first
+}
+
+func buildTree(report []int) [][]int {
+	lines := [][]int{report}
+	current := lines[0]
+	for !allZeroes(current) {
+		next := make([]int, 0, len(current)-1)
+		for i := 0; i < len(current)-1; i++ {
+			next = append(next, (current[i+1] - current[i]))
+		}
+
+		current = next
+		lines = append(lines, current)
+	}
+
+	return lines
+}
+
+func allZeroes(s []int) bool {
+	for _, v := range s {
+		if v != 0 {
+			return false
+		}
+	}
+
+	return true
 }
