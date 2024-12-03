fc3f0b9f1bb2132f50d40eccbbc0dad96d2694fe
David Sedláček <david.littlefarmer@gmail.com>
2024-12-03 11:39:33 +0100

    24/d03

--- 2024/go/days/d03/main.go
+++ 2024/go/days/d03/main.go
@@ -1,7 +1,10 @@
 package d03
 
 import (
+	"advent-of-code/helpers"
 	"fmt"
+	"regexp"
+	"strings"
 	"time"
 )
 
@@ -15,7 +18,16 @@ func Run(input []string) (string, time.Duration, string, time.Duration) {
 func PartOne(input []string) (string, time.Duration) {
 	start := time.Now()
 
-	result := parseInput(input)
+	in := parseInput(input)
+	dos := []Do{
+		{
+			start: 0,
+			end:   len(in) - 1,
+			mode:  true,
+		},
+	}
+
+	result := calculateMulsInMemory(in, dos)
 
 	return fmt.Sprint(result), time.Since(start)
 }
@@ -23,11 +35,82 @@ func PartOne(input []string) (string, time.Duration) {
 func PartTwo(input []string) (string, time.Duration) {
 	start := time.Now()
 
-	result := parseInput(input)
+	in := parseInput(input)
+	dos := findAllDos(in)
+	result := calculateMulsInMemory(in, dos)
 
 	return fmt.Sprint(result), time.Since(start)
 }
 
-func parseInput(input []string) (result int) {
+func calculateMulsInMemory(in string, dos []Do) int {
+	var result int
+	for _, idxs := range regexp.MustCompile(`mul\(\d{1,3},\d{1,3}\)`).FindAllStringIndex(in, -1) {
+		nums := strings.Split(in[idxs[0]+4:idxs[1]-1], ",")
+		if checkDo(dos, idxs[0]) {
+			result += helpers.ParseInt(nums[0]) * helpers.ParseInt(nums[1])
+		}
+	}
+
 	return result
+}
+
+func checkDo(dos []Do, index int) bool {
+	for _, d := range dos {
+		if index >= d.start && index <= d.end {
+			return d.mode
+		}
+	}
+
+	return false
+}
+
+type Do struct {
+	start int
+	end   int
+	mode  bool
+}
+
+func findAllDos(in string) []Do {
+	dos := []Do{{start: 0, mode: true}}
+
+	var start int
+	for start < len(in) {
+		doIdx := strings.Index(in[start:], "do()")
+		dontIdx := strings.Index(in[start:], "don't()")
+		if doIdx == -1 && dontIdx == -1 {
+			break
+		}
+
+		if doIdx != -1 {
+			doIdx += start + 4
+		}
+
+		if dontIdx != -1 {
+			dontIdx += start + 7
+		}
+
+		mode := true
+		index := doIdx
+		if dontIdx != -1 && (doIdx == -1 || dontIdx < doIdx) {
+			index = dontIdx
+			mode = false
+		}
+
+		dos = append(dos, Do{start: index, mode: mode})
+		dos[len(dos)-2].end = index - 1
+		start = index
+	}
+
+	dos[len(dos)-1].end = len(in) - 1
+
+	return dos
+}
+
+func parseInput(input []string) string {
+	var in string
+	for _, l := range input {
+		in += l
+	}
+
+	return in
 }
